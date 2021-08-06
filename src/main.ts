b3bab028bb1e7cf4bb244c50dc233aa9df970dac
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-06 19:28:52 +0200

    Show songs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,43 @@
 import { createSongList } from './components/songsList';
 import './style.css';
+import { Song } from './types';
+import { getSong } from './utils/api';
 import { createElement } from './utils/createElement';
+
+// const songs: Song[] = await getSong();
+// const songsArr = songs.map((song) => createSongList(song));
+
+const songsList = createElement('div', {
+  className: 'content-section__list',
+});
+
+const content = createElement('section', {
+  className: 'content-section',
+  childElements: [
+    songsList,
+    createElement('div', {
+      className: 'content-section__chord',
+    }),
+  ],
+});
+
+const input = createElement('input', {
+  className: 'main__input',
+  placeholder: 'Select an artist',
+  oninput: async () => {
+    songsList.innerHTML = '';
+    const searchArtist = input.value;
+    const filteredSongs: Song[] = await getSong(searchArtist);
+    const createdSongsList = filteredSongs.map((song) => {
+      const listenedSong = createSongList(song);
+      listenedSong.onclick = () => {
+        location.href = `http://www.songsterr.com/a/wa/song?id=${song.id}`;
+      };
+      return listenedSong;
+    });
+    songsList.append(...createdSongsList);
+  },
+});
 
 const appContainer = createElement('div', {
   className: 'appContainer',
@@ -16,24 +53,14 @@ const appContainer = createElement('div', {
     }),
     createElement('main', {
       className: 'main',
-      childElements: [
-        createElement('input', {
-          className: 'main__input',
-          placeholder: 'Select an artist',
-        }),
-        createElement('section', {
-          className: 'content-section',
-          childElements: [
-            createSongList(),
-            createElement('div', {
-              className: 'content-section__chord',
-            }),
-          ],
-        }),
-      ],
+      childElements: [input, content],
     }),
   ],
 });
+
+function onHandler() {
+  console.log('huhu');
+}
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
