8d1b0818b8c92042199e86515a32004d596add7b
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-06 19:30:21 +0200

    Delete unused fetch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,15 @@
 import { Song, SongFromAPI } from '../types';
 
-export async function getSong(): Promise<Song[]> {
+export async function getSong(artistName?: string): Promise<Song[]> {
   const response = await fetch(
-    'https://www.songsterr.com/a/ra/songs.json?pattern=Marley'
+    `https://www.songsterr.com/a/ra/songs.json?pattern=${
+      artistName ? artistName : ''
+    }`
   );
   const data: SongFromAPI[] = await response.json();
   const songs: Song[] = data.map((song) => {
     const formattedSong: Song = {
+      id: song.id,
       artistName: song.artist.name,
       songName: song.title,
     };
