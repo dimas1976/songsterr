295e0fdecc45bd749cbe9b72ce1449bccdb821a0
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-09 17:08:14 +0200

    Fix top level bug

--- src/main.ts
+++ src/main.ts
@@ -4,64 +4,70 @@ import { Song } from './types';
 import { getSong } from './utils/api';
 import { createElement } from './utils/createElement';
 
-// const songs: Song[] = await getSong();
-// const songsArr = songs.map((song) => createSongList(song));
+async function run() {
+  const songsList = createElement('div', {
+    className: 'content-section__list',
+    childElements: [
+      createElement('h3', {
+        innerHTML: 'Select an artist!',
+      }),
+    ],
+  });
 
-const songsList = createElement('div', {
-  className: 'content-section__list',
-});
+  const content = createElement('section', {
+    className: 'content-section',
+    childElements: [
+      songsList,
+      createElement('div', {
+        className: 'content-section__chord',
+      }),
+    ],
+  });
 
-const content = createElement('section', {
-  className: 'content-section',
-  childElements: [
-    songsList,
-    createElement('div', {
-      className: 'content-section__chord',
-    }),
-  ],
-});
+  const input = createElement('input', {
+    className: 'main__input',
+    placeholder: 'Select an artist',
+    oninput: () => {
+      songsList.innerHTML = '';
+    },
+    onchange: async () => {
+      const searchArtist = input.value;
+      const filteredSongs: Song[] = await getSong(searchArtist);
+      const createdSongsList = filteredSongs.map((song) => {
+        const listenedSong = createSongList(song);
+        // listenedSong.onclick = () => {
+        //   location.href = `http://www.songsterr.com/a/wa/song?id=${song.id}`;
+        // };
+        return listenedSong;
+      });
+      songsList.append(...createdSongsList);
+    },
+  });
 
-const input = createElement('input', {
-  className: 'main__input',
-  placeholder: 'Select an artist',
-  oninput: () => {
-    songsList.innerHTML = '';
-  },
-  onchange: async () => {
-    const searchArtist = input.value;
-    const filteredSongs: Song[] = await getSong(searchArtist);
-    const createdSongsList = filteredSongs.map((song) => {
-      const listenedSong = createSongList(song);
-      listenedSong.onclick = () => {
-        location.href = `http://www.songsterr.com/a/wa/song?id=${song.id}`;
-      };
-      return listenedSong;
-    });
-    songsList.append(...createdSongsList);
-  },
-});
+  const appContainer = createElement('div', {
+    className: 'appContainer',
+    childElements: [
+      createElement('header', {
+        className: 'header',
+        childElements: [
+          createElement('h1', {
+            className: 'header__heading',
+            innerText: 'Let`s rock',
+          }),
+        ],
+      }),
+      createElement('main', {
+        className: 'main',
+        childElements: [input, content],
+      }),
+    ],
+  });
 
-const appContainer = createElement('div', {
-  className: 'appContainer',
-  childElements: [
-    createElement('header', {
-      className: 'header',
-      childElements: [
-        createElement('h1', {
-          className: 'header__heading',
-          innerText: 'Let`s rock',
-        }),
-      ],
-    }),
-    createElement('main', {
-      className: 'main',
-      childElements: [input, content],
-    }),
-  ],
-});
+  const app = document.querySelector<HTMLDivElement>('#app');
 
-const app = document.querySelector<HTMLDivElement>('#app');
-
-if (app !== null) {
-  app.append(appContainer);
+  if (app !== null) {
+    app.append(appContainer);
+  }
 }
+
+run();
