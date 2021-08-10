8ebfd2fbdcb1f195d2d8456e143d3d24c72d0754
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-10 09:35:28 +0200

    Add condition to output fist eleven songs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,22 @@
 import { Song, SongFromAPI } from '../types';
 
-export async function getSong(artistName?: string): Promise<Song[]> {
+export async function getSongs(artistName?: string): Promise<Song[]> {
   const response = await fetch(
     `https://www.songsterr.com/a/ra/songs.json?pattern=${
       artistName ? artistName : ''
     }`
   );
   const data: SongFromAPI[] = await response.json();
-  const songs: Song[] = data.map((song) => {
-    const formattedSong: Song = {
-      id: song.id,
-      artistName: song.artist.name,
-      songName: song.title,
-    };
-    return formattedSong;
+  const songs: Song[] = data.map((song, index) => {
+    if (index < 11) {
+      const formattedSong: Song = {
+        id: song.id,
+        artistName: song.artist.name,
+        songName: song.title,
+      };
+      return formattedSong;
+    }
+    return;
   });
-
   return songs;
 }
